f7c68190b813bef6c8561aa1bb01e4ca2560ad62
aster <aster@vers.site>
2012-03-18 12:22:51 +0000

    🎁 Add `Rect` and `Color` to `spark-core`
    Provide shared geometry and RGBA helpers for render and widget crates.

--- projects/engine/spark-core/src/lib.rs
+++ projects/engine/spark-core/src/lib.rs
@@ -17,6 +17,52 @@ impl Vec2 {
     }
 }
 
+/// 轴对齐矩形（屏幕或世界坐标由调用方约定）。
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+}
+
+impl Rect {
+    pub const fn new(x: f32, y: f32, w: f32, h: f32) -> Self {
+        Self { x, y, w, h }
+    }
+
+    pub fn contains(&self, p: Vec2) -> bool {
+        p.x >= self.x && p.y >= self.y && p.x < self.x + self.w && p.y < self.y + self.h
+    }
+
+    pub fn center(self) -> Vec2 {
+        Vec2::new(self.x + self.w * 0.5, self.y + self.h * 0.5)
+    }
+}
+
+/// RGBA，分量 0.0–1.0。
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+impl Color {
+    pub const fn rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
+        Self { r, g, b, a }
+    }
+
+    pub const fn rgb(r: f32, g: f32, b: f32) -> Self {
+        Self::rgba(r, g, b, 1.0)
+    }
+
+    pub fn to_array(self) -> [f32; 4] {
+        [self.r, self.g, self.b, self.a]
+    }
+}
+
 /// 引擎级错误根类型。
 #[derive(Debug, Error)]
 pub enum SparkError {
