4981460937000ac00c83c996e545325f3367d8aa
aster <aster@vers.site>
2012-04-06 08:10:22 +0000

    ✨ Add script plugins for Live2D and Steam
    Introduce `spark-plugin` registry for VM natives plus swappable Live2D and Steam backends aimed at scripting hosts.

---
+++ projects/engine/spark-plugin-steam/src/plugin.rs
@@ -0,0 +1,275 @@
+//! [`SteamPlugin`]：实现 `spark-plugin::Plugin`。
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use spark_gc::{GcObject, Value};
+use spark_plugin::{Plugin, PluginInfo};
+use spark_vm::{NativeCtx, Vm, VmError};
+
+use crate::backend::NullSteamBackend;
+use crate::runtime::SteamRuntime;
+
+/// 脚本侧原生名。
+pub const STEAM_NATIVES: &[&str] = &[
+    "steam_is_available",
+    "steam_app_id",
+    "steam_user_name",
+    "steam_achievement_unlock",
+    "steam_achievement_unlocked",
+    "steam_achievement_clear",
+    "steam_stat_get",
+    "steam_stat_set",
+    "steam_stats_store",
+    "steam_cloud_read",
+    "steam_cloud_write",
+    "steam_cloud_delete",
+    "steam_overlay_open_url",
+];
+
+pub struct SteamPlugin {
+    runtime: Rc<RefCell<SteamRuntime>>,
+}
+
+impl SteamPlugin {
+    pub fn new(runtime: Rc<RefCell<SteamRuntime>>) -> Self {
+        Self { runtime }
+    }
+
+    pub fn with_null_backend() -> Self {
+        Self::new(SteamRuntime::new(Box::new(NullSteamBackend::default())))
+    }
+
+    pub fn with_null_backend_app(app_id: u32, user_name: impl Into<String>) -> Self {
+        Self::new(SteamRuntime::new(Box::new(NullSteamBackend::new(
+            app_id, user_name,
+        ))))
+    }
+
+    pub fn runtime(&self) -> &Rc<RefCell<SteamRuntime>> {
+        &self.runtime
+    }
+}
+
+impl Plugin for SteamPlugin {
+    fn info(&self) -> PluginInfo {
+        PluginInfo {
+            id: "steam",
+            version: "0.1.0",
+            description: "Steam 成就 / 统计 / 云文件（脚本插件）",
+        }
+    }
+
+    fn native_names(&self) -> &'static [&'static str] {
+        STEAM_NATIVES
+    }
+
+    fn install(&mut self, vm: &mut Vm) {
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_is_available", move |_ctx, _args| {
+            Ok(Value::Bool(rt.borrow().backend.is_available()))
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_app_id", move |_ctx, _args| {
+            Ok(Value::Number(rt.borrow().backend.app_id() as f64))
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_user_name", move |ctx, _args| {
+            let name = rt.borrow().backend.user_name();
+            Ok(ctx.heap.alloc_string(name))
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_achievement_unlock", move |ctx, args| {
+            let id = arg_string(ctx, &args, 0)?;
+            rt.borrow_mut()
+                .backend
+                .unlock_achievement(&id)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Null)
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_achievement_unlocked", move |ctx, args| {
+            let id = arg_string(ctx, &args, 0)?;
+            let ok = rt
+                .borrow()
+                .backend
+                .is_achievement_unlocked(&id)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Bool(ok))
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_achievement_clear", move |ctx, args| {
+            let id = arg_string(ctx, &args, 0)?;
+            rt.borrow_mut()
+                .backend
+                .clear_achievement(&id)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Null)
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_stat_get", move |ctx, args| {
+            let name = arg_string(ctx, &args, 0)?;
+            let v = rt
+                .borrow()
+                .backend
+                .get_stat(&name)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Number(v as f64))
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_stat_set", move |ctx, args| {
+            let name = arg_string(ctx, &args, 0)?;
+            let value = arg_f32(&args, 1)?;
+            rt.borrow_mut()
+                .backend
+                .set_stat(&name, value)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Null)
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_stats_store", move |_ctx, _args| {
+            rt.borrow_mut()
+                .backend
+                .store_stats()
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Null)
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_cloud_read", move |ctx, args| {
+            let path = arg_string(ctx, &args, 0)?;
+            match rt
+                .borrow()
+                .backend
+                .cloud_read(&path)
+                .map_err(|e| VmError::Message(e.to_string()))?
+            {
+                Some(s) => Ok(ctx.heap.alloc_string(s)),
+                None => Ok(Value::Null),
+            }
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_cloud_write", move |ctx, args| {
+            let path = arg_string(ctx, &args, 0)?;
+            let data = arg_string(ctx, &args, 1)?;
+            rt.borrow_mut()
+                .backend
+                .cloud_write(&path, &data)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Null)
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_cloud_delete", move |ctx, args| {
+            let path = arg_string(ctx, &args, 0)?;
+            let ok = rt
+                .borrow_mut()
+                .backend
+                .cloud_delete(&path)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Bool(ok))
+        });
+
+        let rt = Rc::clone(&self.runtime);
+        vm.register_native("steam_overlay_open_url", move |ctx, args| {
+            let url = arg_string(ctx, &args, 0)?;
+            rt.borrow_mut()
+                .backend
+                .overlay_open_url(&url)
+                .map_err(|e| VmError::Message(e.to_string()))?;
+            Ok(Value::Null)
+        });
+    }
+}
+
+fn arg_f32(args: &[Value], i: usize) -> Result<f32, VmError> {
+    args.get(i)
+        .and_then(|v| v.as_number())
+        .map(|n| n as f32)
+        .ok_or_else(|| VmError::Message("需要 number 参数".into()))
+}
+
+fn arg_string(ctx: &NativeCtx<'_>, args: &[Value], i: usize) -> Result<String, VmError> {
+    let v = args
+        .get(i)
+        .ok_or_else(|| VmError::Message("缺少字符串参数".into()))?;
+    match v {
+        Value::Handle(h) => match ctx.heap.get(*h) {
+            Ok(GcObject::String(s)) => Ok(s.clone()),
+            _ => Err(VmError::Message("期望字符串".into())),
+        },
+        Value::Number(n) => Ok(n.to_string()),
+        Value::Bool(b) => Ok(b.to_string()),
+        Value::Null => Ok(String::new()),
+        _ => Err(VmError::Message("期望字符串".into())),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use spark_plugin::PluginRegistry;
+    use spark_vm::{FuncProto, Module, Op, StdHost};
+
+    #[test]
+    fn null_backend_achievements_and_cloud() {
+        let plugin = SteamPlugin::with_null_backend_app(480, "tester");
+        let rt = Rc::clone(plugin.runtime());
+        assert!(!rt.borrow().backend.is_available());
+        assert_eq!(rt.borrow().backend.app_id(), 480);
+        rt.borrow_mut()
+            .backend
+            .unlock_achievement("ACH_FIRST")
+            .unwrap();
+        assert!(rt
+            .borrow()
+            .backend
+            .is_achievement_unlocked("ACH_FIRST")
+            .unwrap());
+        rt.borrow_mut().backend.set_stat("kills", 3.0).unwrap();
+        assert!((rt.borrow().backend.get_stat("kills").unwrap() - 3.0).abs() < 1e-5);
+        rt.borrow_mut()
+            .backend
+            .cloud_write("save.txt", "hello")
+            .unwrap();
+        assert_eq!(
+            rt.borrow().backend.cloud_read("save.txt").unwrap().as_deref(),
+            Some("hello")
+        );
+    }
+
+    #[test]
+    fn install_and_call_app_id() {
+        let mut reg = PluginRegistry::new();
+        reg.register(Box::new(SteamPlugin::with_null_backend_app(1234, "u")))
+            .unwrap();
+        assert!(reg.contains("steam"));
+
+        let mut module = Module {
+            functions: vec![],
+            entry: 0,
+            native_names: Vec::new(),
+        };
+        let ni = module.intern_native("steam_app_id");
+        let mut f = FuncProto::new("__main", 0);
+        f.emit(Op::CallNative);
+        f.emit_u16(ni);
+        f.emit_u8(0);
+        f.emit(Op::Return);
+        module.functions.push(f);
+
+        let mut vm = spark_vm::Vm::new(module);
+        reg.install_all(&mut vm);
+        let v = vm.run(&mut StdHost).unwrap();
+        assert_eq!(v.as_number(), Some(1234.0));
+    }
+}
---
+++ projects/engine/spark-plugin/src/lib.rs
@@ -0,0 +1,183 @@
+//! Spark **脚本插件**机制。
+//!
+//! # 定位
+//!
+//! - **给 scripting / `spark-vm` 用**：宿主在启动时把插件装进 VM，脚本经 `CallNative` 调用。
+//! - **不是** Rust 扩展点：Rust 代码需要能力时直接 `path` / crates.io 依赖对应 crate 即可。
+//!
+//! 插件只负责声明元信息，并把原生函数注册到 [`Vm`]。
+
+use spark_vm::Vm;
+use thiserror::Error;
+
+/// 插件元信息（稳定 id，供注册表去重）。
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct PluginInfo {
+    pub id: &'static str,
+    pub version: &'static str,
+    pub description: &'static str,
+}
+
+/// 可安装到 VM 的脚本插件。
+///
+/// 实现方持有内部状态（通常 `Rc<RefCell<_>>`），在 [`Plugin::install`] 里
+/// `register_native` 一批供脚本调用的函数。
+pub trait Plugin {
+    fn info(&self) -> PluginInfo;
+
+    /// 本插件将注册的原生名（`'static`，供脚本前端编译期声明）。
+    fn native_names(&self) -> &'static [&'static str];
+
+    /// 向 VM 注册原生函数。可多次调用（每个模组 VM 各装一次）。
+    fn install(&mut self, vm: &mut Vm);
+}
+
+#[derive(Debug, Error)]
+pub enum PluginError {
+    #[error("插件 id 已注册：`{0}`")]
+    DuplicateId(String),
+    #[error("未注册插件：`{0}`")]
+    NotFound(String),
+}
+
+/// 汇总若干插件的原生名（去重保序）。
+pub fn collect_native_names(plugins: &[&dyn Plugin]) -> Vec<&'static str> {
+    let mut out = Vec::new();
+    for p in plugins {
+        for n in p.native_names() {
+            if !out.iter().any(|x| *x == *n) {
+                out.push(*n);
+            }
+        }
+    }
+    out
+}
+
+/// 插件注册表：宿主持有，按需装进各个脚本 VM。
+#[derive(Default)]
+pub struct PluginRegistry {
+    plugins: Vec<Box<dyn Plugin>>,
+}
+
+impl PluginRegistry {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn register(&mut self, plugin: Box<dyn Plugin>) -> Result<(), PluginError> {
+        let id = plugin.info().id;
+        if self.plugins.iter().any(|p| p.info().id == id) {
+            return Err(PluginError::DuplicateId(id.into()));
+        }
+        tracing::info!(
+            plugin = id,
+            version = plugin.info().version,
+            "已注册脚本插件"
+        );
+        self.plugins.push(plugin);
+        Ok(())
+    }
+
+    pub fn len(&self) -> usize {
+        self.plugins.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.plugins.is_empty()
+    }
+
+    pub fn infos(&self) -> Vec<PluginInfo> {
+        self.plugins.iter().map(|p| p.info()).collect()
+    }
+
+    pub fn contains(&self, id: &str) -> bool {
+        self.plugins.iter().any(|p| p.info().id == id)
+    }
+
+    /// 供脚本前端 `compile_with(..., natives)` 使用的名字表。
+    pub fn native_names(&self) -> Vec<&'static str> {
+        let mut out = Vec::new();
+        for p in &self.plugins {
+            for n in p.native_names() {
+                if !out.iter().any(|x| *x == *n) {
+                    out.push(*n);
+                }
+            }
+        }
+        out
+    }
+
+    /// 把全部插件的原生函数装进指定 VM。
+    pub fn install_all(&mut self, vm: &mut Vm) {
+        for p in &mut self.plugins {
+            p.install(vm);
+        }
+    }
+
+    /// 只安装指定 id。
+    pub fn install_one(&mut self, id: &str, vm: &mut Vm) -> Result<(), PluginError> {
+        let Some(p) = self.plugins.iter_mut().find(|p| p.info().id == id) else {
+            return Err(PluginError::NotFound(id.into()));
+        };
+        p.install(vm);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use spark_gc::Value;
+    use spark_vm::{FuncProto, Module, NativeCtx, Op, StdHost};
+
+    struct EchoPlugin;
+
+    impl Plugin for EchoPlugin {
+        fn info(&self) -> PluginInfo {
+            PluginInfo {
+                id: "echo",
+                version: "0.1.0",
+                description: "测试回声",
+            }
+        }
+
+        fn native_names(&self) -> &'static [&'static str] {
+            &["echo_ping"]
+        }
+
+        fn install(&mut self, vm: &mut Vm) {
+            vm.register_native("echo_ping", |_ctx: &mut NativeCtx<'_>, args: Vec<Value>| {
+                Ok(args.into_iter().next().unwrap_or(Value::Null))
+            });
+        }
+    }
+
+    #[test]
+    fn register_and_call() {
+        let mut reg = PluginRegistry::new();
+        reg.register(Box::new(EchoPlugin)).unwrap();
+        assert!(reg.contains("echo"));
+        assert_eq!(reg.native_names(), ["echo_ping"]);
+
+        let mut module = Module {
+            functions: vec![],
+            entry: 0,
+            native_names: Vec::new(),
+        };
+        let ni = module.intern_native("echo_ping");
+        let mut f = FuncProto::new("__main", 0);
+        let c = f.add_const_number(7.0);
+        f.emit(Op::LoadConst);
+        f.emit_u16(c);
+        f.emit(Op::CallNative);
+        f.emit_u16(ni);
+        f.emit_u8(1);
+        f.emit(Op::Return);
+        module.functions.push(f);
+
+        let mut vm = Vm::new(module);
+        reg.install_all(&mut vm);
+        let v = vm.run(&mut StdHost).unwrap();
+        assert_eq!(v.as_number(), Some(7.0));
+    }
+}
