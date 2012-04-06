4547de8bdd40e60de07eea2e185bb17157978204
aster <aster@vers.site>
2012-04-06 00:20:57 +0000

    ✨ Add Symphonia media stack for audio and video
    Add `spark-media` demux and decode shared by `spark-audio` playback and `spark-video` packet pumping.

---
+++ projects/engine/spark-media/src/decode.rs
@@ -0,0 +1,224 @@
+//! 音频轨 → 交错 f32 PCM。
+
+use std::fs::File;
+use std::path::Path;
+
+use symphonia::core::audio::AudioBufferRef;
+use symphonia::core::codecs::{DecoderOptions, CODEC_TYPE_NULL};
+use symphonia::core::errors::Error as SymError;
+use symphonia::core::formats::FormatOptions;
+use symphonia::core::io::{MediaSourceStream, MediaSourceStreamOptions};
+use symphonia::core::meta::MetadataOptions;
+use symphonia::core::probe::Hint;
+use symphonia::default::{get_codecs, get_probe};
+
+use crate::MediaError;
+
+/// 解码后的 PCM（交错，范围约 -1..=1）。
+#[derive(Debug, Clone)]
+pub struct PcmAudio {
+    pub sample_rate: u32,
+    pub channels: u16,
+    pub samples: Vec<f32>,
+}
+
+impl PcmAudio {
+    pub fn frame_count(&self) -> usize {
+        if self.channels == 0 {
+            return 0;
+        }
+        self.samples.len() / self.channels as usize
+    }
+
+    pub fn duration_secs(&self) -> f64 {
+        self.frame_count() as f64 / self.sample_rate.max(1) as f64
+    }
+}
+
+/// 基于 Symphonia 的音频解码器。
+pub struct AudioDecoder;
+
+impl AudioDecoder {
+    /// 解码文件中默认（或指定）音频轨为整段 PCM。
+    pub fn decode_path(
+        path: impl AsRef<Path>,
+        track_id: Option<u32>,
+    ) -> Result<PcmAudio, MediaError> {
+        let path = path.as_ref();
+        let file = File::open(path).map_err(|e| {
+            MediaError::Message(format!("打开音频失败 {}: {e}", path.display()))
+        })?;
+        let mut hint = Hint::new();
+        if let Some(ext) = path.extension().and_then(|e| e.to_str()) {
+            hint.with_extension(ext);
+        }
+        Self::decode_mss(
+            MediaSourceStream::new(Box::new(file), MediaSourceStreamOptions::default()),
+            hint,
+            track_id,
+        )
+    }
+
+    pub fn decode_bytes(
+        bytes: impl Into<Vec<u8>>,
+        track_id: Option<u32>,
+    ) -> Result<PcmAudio, MediaError> {
+        let cursor = std::io::Cursor::new(bytes.into());
+        Self::decode_mss(
+            MediaSourceStream::new(Box::new(cursor), MediaSourceStreamOptions::default()),
+            Hint::new(),
+            track_id,
+        )
+    }
+
+    fn decode_mss(
+        mss: MediaSourceStream,
+        hint: Hint,
+        track_id: Option<u32>,
+    ) -> Result<PcmAudio, MediaError> {
+        let probed = get_probe().format(
+            &hint,
+            mss,
+            &FormatOptions {
+                enable_gapless: true,
+                ..Default::default()
+            },
+            &MetadataOptions::default(),
+        )?;
+        let mut format = probed.format;
+
+        let track = if let Some(id) = track_id {
+            format
+                .tracks()
+                .iter()
+                .find(|t| t.id == id)
+                .ok_or(MediaError::NoAudioTrack)?
+                .clone()
+        } else {
+            format
+                .tracks()
+                .iter()
+                .find(|t| {
+                    t.codec_params.codec != CODEC_TYPE_NULL
+                        && t.codec_params.sample_rate.is_some()
+                })
+                .cloned()
+                .ok_or(MediaError::NoAudioTrack)?
+        };
+
+        let sample_rate = track
+            .codec_params
+            .sample_rate
+            .ok_or(MediaError::NoAudioTrack)?;
+        let channels = track
+            .codec_params
+            .channels
+            .map(|c| c.count() as u16)
+            .unwrap_or(1)
+            .max(1);
+
+        let mut decoder = get_codecs().make(&track.codec_params, &DecoderOptions::default())?;
+
+        let mut samples = Vec::new();
+        let track_id = track.id;
+
+        loop {
+            let packet = match format.next_packet() {
+                Ok(p) => p,
+                Err(SymError::ResetRequired) => continue,
+                Err(SymError::IoError(e)) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
+                Err(e) => return Err(e.into()),
+            };
+            if packet.track_id() != track_id {
+                continue;
+            }
+            match decoder.decode(&packet) {
+                Ok(buf) => append_f32(&mut samples, &buf),
+                Err(SymError::DecodeError(_)) => continue,
+                Err(e) => return Err(e.into()),
+            }
+        }
+
+        Ok(PcmAudio {
+            sample_rate,
+            channels,
+            samples,
+        })
+    }
+}
+
+fn append_f32(out: &mut Vec<f32>, buf: &AudioBufferRef<'_>) {
+    fn push_planes<T: Copy>(out: &mut Vec<f32>, planes: &[&[T]], map: impl Fn(T) -> f32) {
+        let frames = planes.first().map(|p| p.len()).unwrap_or(0);
+        out.reserve(frames * planes.len());
+        for i in 0..frames {
+            for plane in planes {
+                out.push(map(plane[i]));
+            }
+        }
+    }
+
+    match buf {
+        AudioBufferRef::F32(b) => push_planes(out, b.planes().planes(), |v| v),
+        AudioBufferRef::U8(b) => push_planes(out, b.planes().planes(), |v| v as f32 / 128.0 - 1.0),
+        AudioBufferRef::U16(b) => {
+            push_planes(out, b.planes().planes(), |v| v as f32 / 32768.0 - 1.0)
+        }
+        AudioBufferRef::U24(b) => push_planes(out, b.planes().planes(), |v| {
+            v.inner() as f32 / 8_388_608.0 - 1.0
+        }),
+        AudioBufferRef::U32(b) => {
+            push_planes(out, b.planes().planes(), |v| v as f32 / 2_147_483_648.0 - 1.0)
+        }
+        AudioBufferRef::S8(b) => push_planes(out, b.planes().planes(), |v| v as f32 / 128.0),
+        AudioBufferRef::S16(b) => push_planes(out, b.planes().planes(), |v| v as f32 / 32768.0),
+        AudioBufferRef::S24(b) => {
+            push_planes(out, b.planes().planes(), |v| v.inner() as f32 / 8_388_608.0)
+        }
+        AudioBufferRef::S32(b) => {
+            push_planes(out, b.planes().planes(), |v| v as f32 / 2_147_483_648.0)
+        }
+        AudioBufferRef::F64(b) => push_planes(out, b.planes().planes(), |v| v as f32),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// 最小合法 PCM WAV（单声道 8bit 44100Hz，若干样本）。
+    fn tiny_wav() -> Vec<u8> {
+        let samples: Vec<u8> = (0..64).map(|i| 128u8.wrapping_add(i)).collect();
+        let data_len = samples.len() as u32;
+        let mut v = Vec::new();
+        v.extend_from_slice(b"RIFF");
+        v.extend_from_slice(&(36 + data_len).to_le_bytes());
+        v.extend_from_slice(b"WAVE");
+        v.extend_from_slice(b"fmt ");
+        v.extend_from_slice(&16u32.to_le_bytes());
+        v.extend_from_slice(&1u16.to_le_bytes()); // PCM
+        v.extend_from_slice(&1u16.to_le_bytes()); // mono
+        v.extend_from_slice(&44100u32.to_le_bytes());
+        v.extend_from_slice(&(44100u32).to_le_bytes()); // byte rate
+        v.extend_from_slice(&1u16.to_le_bytes()); // block align
+        v.extend_from_slice(&8u16.to_le_bytes()); // bits
+        v.extend_from_slice(b"data");
+        v.extend_from_slice(&data_len.to_le_bytes());
+        v.extend_from_slice(&samples);
+        v
+    }
+
+    #[test]
+    fn decode_wav_bytes() {
+        let pcm = AudioDecoder::decode_bytes(tiny_wav(), None).unwrap();
+        assert_eq!(pcm.sample_rate, 44100);
+        assert_eq!(pcm.channels, 1);
+        assert!(!pcm.samples.is_empty());
+    }
+
+    #[test]
+    fn probe_wav_audio_track() {
+        let info = crate::probe_bytes(&tiny_wav()).unwrap();
+        assert!(info.default_audio().is_some());
+    }
+}
---
+++ projects/engine/spark-video/src/lib.rs
@@ -0,0 +1,116 @@
+//! 视频框架：基于 `spark-media`（Symphonia）解复用视频轨。
+//!
+//! 当前交付轨元数据与压缩包泵；像素帧解码留给渲染/硬件路径。
+//! **无**过场剧本或游戏镜头语义。
+
+use std::path::Path;
+use std::time::Duration;
+
+use spark_core::SparkError;
+use spark_media::{
+    MediaPacket, MediaReader, PacketKind, VideoTrackInfo,
+};
+
+/// 打开中的视频剪辑（可含同文件音频轨 ID，供上层 AV 同步）。
+pub struct VideoClip {
+    reader: MediaReader,
+    video: VideoTrackInfo,
+    audio_track_id: Option<u32>,
+}
+
+impl VideoClip {
+    pub fn open(path: impl AsRef<Path>) -> Result<Self, SparkError> {
+        let reader = MediaReader::open_path(path).map_err(SparkError::from)?;
+        let video = reader
+            .default_video()
+            .cloned()
+            .ok_or_else(|| SparkError::Message("媒体中无视频轨".into()))?;
+        let audio_track_id = reader.default_audio().map(|a| a.track_id);
+        Ok(Self {
+            reader,
+            video,
+            audio_track_id,
+        })
+    }
+
+    pub fn open_bytes(bytes: impl Into<Vec<u8>>) -> Result<Self, SparkError> {
+        let reader = MediaReader::open_bytes(bytes).map_err(SparkError::from)?;
+        let video = reader
+            .default_video()
+            .cloned()
+            .ok_or_else(|| SparkError::Message("媒体中无视频轨".into()))?;
+        let audio_track_id = reader.default_audio().map(|a| a.track_id);
+        Ok(Self {
+            reader,
+            video,
+            audio_track_id,
+        })
+    }
+
+    pub fn video_info(&self) -> &VideoTrackInfo {
+        &self.video
+    }
+
+    pub fn audio_track_id(&self) -> Option<u32> {
+        self.audio_track_id
+    }
+
+    pub fn width(&self) -> u32 {
+        self.video.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.video.height
+    }
+
+    /// 根据 time_base 把时间戳转为时长。
+    pub fn timestamp_to_duration(&self, ts: u64) -> Duration {
+        let numer = self.video.time_base_numer as u128;
+        let denom = self.video.time_base_denom.max(1) as u128;
+        let nanos = ts as u128 * numer * 1_000_000_000u128 / denom;
+        Duration::from_nanos(nanos.min(u64::MAX as u128) as u64)
+    }
+
+    pub fn seek_seconds(&mut self, seconds: f64) -> Result<(), SparkError> {
+        self.reader.seek_seconds(seconds).map_err(SparkError::from)
+    }
+
+    /// 下一视频包；到 EOS 返回 `None`。
+    pub fn next_video_packet(&mut self) -> Result<Option<MediaPacket>, SparkError> {
+        loop {
+            match self
+                .reader
+                .next_packet(Some(self.video.track_id))
+                .map_err(SparkError::from)?
+            {
+                None => return Ok(None),
+                Some(p) if p.kind == PacketKind::Video || p.track_id == self.video.track_id => {
+                    return Ok(Some(p));
+                }
+                Some(_) => continue,
+            }
+        }
+    }
+
+    /// 下一任意包（视频或同容器音频），便于上层交错调度。
+    pub fn next_packet(&mut self) -> Result<Option<MediaPacket>, SparkError> {
+        self.reader.next_packet(None).map_err(SparkError::from)
+    }
+}
+
+/// 占位：尚未像素解码的视频帧句柄（携带压缩包）。
+#[derive(Debug, Clone)]
+pub struct EncodedFrame {
+    pub pts: Duration,
+    pub packet: MediaPacket,
+}
+
+impl VideoClip {
+    pub fn next_encoded_frame(&mut self) -> Result<Option<EncodedFrame>, SparkError> {
+        let Some(packet) = self.next_video_packet()? else {
+            return Ok(None);
+        };
+        let pts = self.timestamp_to_duration(packet.ts);
+        Ok(Some(EncodedFrame { pts, packet }))
+    }
+}
