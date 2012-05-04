cf43244fab67bdd3ce1c5ab2c90eaa452cc6decd
aster <aster@vers.site>
2012-05-04 12:44:45 +0000

    ♻️ Re-export 3D AABB sweep from `spark-physics`
    Surface `aabb_sweep` and `aabb_sweep_resolve` next to the 2D world API.
    Keep the algorithms owned by `spark-geometry`.

--- projects/foundation/spark-physics/src/lib.rs
+++ projects/foundation/spark-physics/src/lib.rs
@@ -1,6 +1,7 @@
-//! Spark **2D 物理框架**（无游戏语义）。
+//! Spark **物理框架**（无游戏语义）。
 //!
-//! 提供 AABB 刚体、均匀网格宽相、圆/盒窄相委托 `spark-geometry`，以及带重力的固定步。
+//! 2D：AABB 刚体、均匀网格宽相、圆/盒窄相委托 `spark-geometry`，以及带重力的固定步。
+//! 3D：扫掠原语直接再导出 `spark-geometry`（[`aabb_sweep`] / [`aabb_sweep_resolve`]）。
 //! **不**提供行星重力模式、方块碰撞表或玩法材质。
 
 mod body;
@@ -9,6 +10,7 @@ mod world;
 
 pub use body::{BodyId, BodyKind, Collider2, RigidBody2};
 pub use broadphase::{Broadphase, UniformGrid};
+pub use spark_geometry::{aabb_sweep, aabb_sweep_resolve, Aabb3, SweepHit, Vec3};
 pub use world::{Contact, PhysicsConfig, PhysicsWorld};
 
 use spark_core::SparkError;
