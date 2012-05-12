bfa33c0bf29cd2ea6e1b2c2c2cdaa8bd5efb6eda
aster <aster@vers.site>
2012-05-12 15:16:41 +0000

    🔄 Replace `oak-valkyrie` frontend with hand-written subset parser
    Route `spark-script-valkyrie` through a recursive-descent subset so top-level native calls like `register_block` compile. Drop oak `Builder` probes that no longer parse expression statements.

---
+++ projects/scripting/spark-script-valkyrie/tests/parse_subset.rs
@@ -0,0 +1,22 @@
+//! 探针：确认手写 Valkyrie 子集能解析常见语句形态。
+
+#[test]
+fn probe_valkyrie_sources() {
+    for src in [
+        "return 40 + 2",
+        "40 + 2",
+        "let x = 40",
+        "micro f() { return 1 }",
+        "micro add(x, y) { return x + y }",
+        r#"register_block(1, "a:b", "名", "t.png", 1, 1, 30, "none")"#,
+        "",
+    ] {
+        let r = spark_script_valkyrie::parse(src);
+        eprintln!(
+            "SRC={src:?} ok={} err={:?}",
+            r.is_ok(),
+            r.as_ref().err()
+        );
+        assert!(r.is_ok(), "parse failed for {src:?}: {r:?}");
+    }
+}
