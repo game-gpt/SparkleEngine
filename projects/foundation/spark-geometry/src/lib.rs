624e593c0cee8b48c24c7cff02148714191761a3
aster <aster@vers.site>
2012-05-13 11:33:53 +0000

    🚀 Add `Quat` and `Trs` to `spark-geometry`
    Introduce unit quaternions with nlerp/slerp and TRS composition as T * R * S.
    Extend `Mat4` with scaling and `from_trs` for skinned animation prep.

--- projects/foundation/spark-geometry/src/lib.rs
+++ projects/foundation/spark-geometry/src/lib.rs
@@ -1,7 +1,7 @@
 //! Spark 几何库（无游戏语义）。
 //!
 //! 2D：[`Vec2`] / [`Rect`] 在 `spark-core`；本 crate 提供运算扩展与相交。
-//! 3D：[`Vec3`] / [`Mat4`] / [`Aabb3`] / [`Ray3`] 供渲染与体素局部坐标共用。
+//! 3D：[`Vec3`] / [`Mat4`] / [`Quat`] / [`Trs`] / [`Aabb3`] / [`Ray3`] 供渲染与体素局部坐标共用。
 
 mod aabb3;
 mod circle;
@@ -9,9 +9,11 @@ mod collide;
 mod line;
 mod mat4;
 mod polygon;
+mod quat;
 mod ray3;
 mod sweep3;
 mod transform;
+mod trs;
 mod vec3;
 mod vec_ext;
 
@@ -24,10 +26,12 @@ pub use collide::{
 pub use line::{LineSegment, Ray};
 pub use mat4::Mat4;
 pub use polygon::Polygon;
+pub use quat::Quat;
 pub use ray3::{ray_aabb, ray_voxel, ray_voxel_dda, Ray3, VoxelHit};
 pub use spark_core::{Rect, Vec2};
 pub use sweep3::{aabb_sweep, aabb_sweep_resolve, SweepHit};
 pub use transform::Transform2;
+pub use trs::Trs;
 pub use vec3::Vec3;
 pub use vec_ext::Vec2Ext;
 
