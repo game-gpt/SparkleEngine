902eb1a6711771bb501aff0cf30b9b6a46a52967
aster <aster@vers.site>
2012-06-22 12:28:09 +0000

    ✨ Add localization manifest check and pseudo tools
    Introduce `LocalizationManifest`, build-time `check_document` / `check_locale_set`, and `generate_pseudo` for accent long and `RTL` mirror locales.

--- projects/foundation/spark-localization/src/lib.rs
+++ projects/foundation/spark-localization/src/lib.rs
@@ -8,13 +8,17 @@
 //! - **负责**：BCP 47、消息求值契约、回退链、切换事务数据、伪本地化钩子位。
 //! - **不负责**：机器翻译、玩法权威状态、已格式化文本的网络/存档权威。
 
+mod check;
 mod diagnostic;
 mod document;
 mod locale;
+mod manifest;
 mod message;
+mod pseudo;
 mod snapshot;
 mod text;
 
+pub use check::{CheckIssue, CheckIssueKind, CheckReport, check_document, check_locale_set};
 pub use diagnostic::{DiagnosticFlags, DiagnosticRecord, MessageDiagnostic};
 pub use document::{
     ArgumentFormat, LocalizationDocument, MessageDefinition, MessageName, MessageNode, SelectKind,
@@ -22,9 +26,11 @@ pub use document::{
 pub use locale::{
     LocaleId, LocaleParseError, LocaleRequest, TextDirection, build_fallback_chain, negotiate,
 };
+pub use manifest::{LocaleEntry, LocalizationManifest, NamespaceOwner};
 pub use message::{
     AttributeId, MessageArgs, MessageDateTime, MessageDecimal, MessageDuration, MessageId,
     MessageRef, MessageValue, NamespaceId,
 };
+pub use pseudo::{PseudoKind, generate_pseudo};
 pub use snapshot::{LocaleChanged, LocaleSnapshot, Localizer};
 pub use text::{LocalizedContent, LocalizedText, LocalizedToken};
---
+++ projects/foundation/spark-localization/src/pseudo.rs
@@ -0,0 +1,150 @@
+//! 伪本地化：扩张、加长与 RTL 镜像，用于暴露硬编码布局假设。
+
+use std::sync::Arc;
+
+use crate::document::{LocalizationDocument, MessageDefinition, MessageNode};
+use crate::locale::LocaleId;
+
+/// 伪 Locale 变体。
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum PseudoKind {
+    /// 重音扩张（保留可读性，拉长字形宽度）。
+    Accents,
+    /// 超长文本（约 1.5×–2×）。
+    Long,
+    /// RTL 镜像包装（加入 RLM / 方向隔离标记）。
+    RtlMirror,
+}
+
+impl PseudoKind {
+    pub fn locale_tag(self) -> &'static str {
+        match self {
+            Self::Accents => "en-XA",
+            Self::Long => "en-XB",
+            Self::RtlMirror => "en-XC",
+        }
+    }
+}
+
+/// 由基线英文（或任意）文档生成伪本地化文档。
+pub fn generate_pseudo(source: &LocalizationDocument, kind: PseudoKind) -> LocalizationDocument {
+    let locale = LocaleId::parse(kind.locale_tag()).expect("pseudo locale tags are valid");
+    let mut out = LocalizationDocument::new(locale, source.namespace.as_str());
+    for (name, def) in &source.messages {
+        out.insert(name.as_str(), transform_definition(def, kind));
+    }
+    out
+}
+
+fn transform_definition(def: &MessageDefinition, kind: PseudoKind) -> MessageDefinition {
+    match def {
+        MessageDefinition::Text(text) => MessageDefinition::Text(Arc::from(transform_text(text, kind))),
+        MessageDefinition::Pattern(nodes) => {
+            MessageDefinition::Pattern(transform_nodes(nodes, kind))
+        }
+        MessageDefinition::Select {
+            argument,
+            kind: select_kind,
+            cases,
+        } => {
+            let mut new_cases = cases.clone();
+            for nodes in new_cases.values_mut() {
+                *nodes = transform_nodes(nodes, kind);
+            }
+            MessageDefinition::Select {
+                argument: argument.clone(),
+                kind: *select_kind,
+                cases: new_cases,
+            }
+        }
+    }
+}
+
+fn transform_nodes(nodes: &[MessageNode], kind: PseudoKind) -> Vec<MessageNode> {
+    nodes
+        .iter()
+        .map(|node| match node {
+            MessageNode::Text(text) => MessageNode::Text(Arc::from(transform_text(text, kind))),
+            MessageNode::Argument { name, format } => MessageNode::Argument {
+                name: name.clone(),
+                format: format.clone(),
+            },
+            MessageNode::MessageRef { name, attribute } => MessageNode::MessageRef {
+                name: name.clone(),
+                attribute: attribute.clone(),
+            },
+            MessageNode::Select {
+                argument,
+                kind: select_kind,
+                cases,
+            } => {
+                let mut new_cases = cases.clone();
+                for child in new_cases.values_mut() {
+                    *child = transform_nodes(child, kind);
+                }
+                MessageNode::Select {
+                    argument: argument.clone(),
+                    kind: *select_kind,
+                    cases: new_cases,
+                }
+            }
+        })
+        .collect()
+}
+
+fn transform_text(input: &str, kind: PseudoKind) -> String {
+    match kind {
+        PseudoKind::Accents => accentuate(input),
+        PseudoKind::Long => {
+            let accented = accentuate(input);
+            format!("[{accented} >>>]")
+        }
+        PseudoKind::RtlMirror => {
+            // U+202E RLO … U+202C PDF：强制视觉镜像，便于发现未声明方向的布局。
+            format!("\u{202E}{input}\u{202C}")
+        }
+    }
+}
+
+fn accentuate(input: &str) -> String {
+    let mut out = String::with_capacity(input.len() * 2);
+    for ch in input.chars() {
+        out.push(match ch {
+            'a' => 'á',
+            'e' => 'é',
+            'i' => 'í',
+            'o' => 'ó',
+            'u' => 'ú',
+            'A' => 'Á',
+            'E' => 'É',
+            'I' => 'Í',
+            'O' => 'Ó',
+            'U' => 'Ú',
+            'c' => 'ç',
+            'C' => 'Ç',
+            'n' => 'ñ',
+            'N' => 'Ñ',
+            other => other,
+        });
+    }
+    out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::locale::LocaleId;
+
+    #[test]
+    fn accents_expand_ascii_vowels() {
+        let mut doc = LocalizationDocument::new(LocaleId::parse("en").unwrap(), "game");
+        doc.insert("menu.continue", MessageDefinition::Text(Arc::from("Continue")));
+        let pseudo = generate_pseudo(&doc, PseudoKind::Accents);
+        let def = pseudo.messages.values().next().unwrap();
+        match def {
+            MessageDefinition::Text(text) => assert_eq!(text.as_ref(), "Çóñtíñúé"),
+            other => panic!("unexpected {other:?}"),
+        }
+        assert_eq!(pseudo.locale.as_str(), "en-XA");
+    }
+}
