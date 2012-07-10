89e6d1d7038406037f9a382551c793eb1aef81f8
aster <aster@vers.site>
2012-07-10 15:11:17 +0000

    ✨ Keep `spark-media` errors on path and kind tokens
    Replace open/decode Display sentences with stable codes and typed rgs.
    Call sites pass io::Error into `MediaError::open_path`.

--- projects/graphics/spark-media/src/lib.rs
+++ projects/graphics/spark-media/src/lib.rs
@@ -14,44 +14,45 @@ pub use demux::{MediaPacket, MediaReader, PacketKind};
 pub use probe::{probe_bytes, probe_path, AudioTrackInfo, MediaInfo, TrackInfo, VideoTrackInfo};
 
 use std::fmt;
+use std::io;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use spark_core::{ErrorArg, SparkError};
+use spark_core::{ErrorArg, ErrorArgs, SparkError};
 
 /// 媒体层结构化错误。`Display` 只输出稳定码。
 #[derive(Debug)]
 pub enum MediaError {
+    /// 打开失败：路径事实 + IO kind 令牌（非 OS 本地化句子）。
     Open {
         path: Option<PathBuf>,
-        detail: String,
+        io_kind: &'static str,
     },
     NoAudioTrack,
     NoVideoTrack,
     UnsupportedCodec { codec: String },
     EndOfStream,
-    Decode { detail: String },
+    /// 解码失败：Symphonia / 容器层稳定 kind 令牌。
+    Decode { kind: &'static str },
 }
 
 impl MediaError {
-    pub fn open(path: impl Into<Option<PathBuf>>, detail: impl Into<String>) -> Self {
+    pub fn open_io(path: impl Into<Option<PathBuf>>, kind: io::ErrorKind) -> Self {
         Self::Open {
             path: path.into(),
-            detail: detail.into(),
+            io_kind: io_kind_token(kind),
         }
     }
 
-    pub fn open_path(path: impl Into<PathBuf>, detail: impl Into<String>) -> Self {
+    pub fn open_path(path: impl Into<PathBuf>, err: &io::Error) -> Self {
         Self::Open {
             path: Some(path.into()),
-            detail: detail.into(),
+            io_kind: io_kind_token(err.kind()),
         }
     }
 
-    pub fn decode(detail: impl Into<String>) -> Self {
-        Self::Decode {
-            detail: detail.into(),
-        }
+    pub fn decode_kind(kind: &'static str) -> Self {
+        Self::Decode { kind }
     }
 
     pub fn code(&self) -> &'static str {
@@ -63,6 +64,53 @@ impl MediaError {
             Self::EndOfStream => "spark.media.end_of_stream",
             Self::Decode { .. } => "spark.media.decode",
         }
+    }
+
+    pub fn args(&self) -> ErrorArgs {
+        match self {
+            Self::Open { path, io_kind } => {
+                let mut args = ErrorArgs::new().with("io_kind", ErrorArg::String(Arc::from(*io_kind)));
+                if let Some(p) = path {
+                    args.insert(
+                        "path",
+                        ErrorArg::Path(Arc::from(p.to_string_lossy().as_ref())),
+                    );
+                }
+                args
+            }
+            Self::UnsupportedCodec { codec } => {
+                ErrorArgs::new().with("codec", ErrorArg::String(Arc::from(codec.as_str())))
+            }
+            Self::Decode { kind } => {
+                ErrorArgs::new().with("kind", ErrorArg::String(Arc::from(*kind)))
+            }
+            _ => ErrorArgs::new(),
+        }
+    }
+}
+
+fn io_kind_token(kind: io::ErrorKind) -> &'static str {
+    use io::ErrorKind::*;
+    match kind {
+        NotFound => "not_found",
+        PermissionDenied => "permission_denied",
+        InvalidData => "invalid_data",
+        UnexpectedEof => "unexpected_eof",
+        AlreadyExists => "already_exists",
+        TimedOut => "timed_out",
+        _ => "other",
+    }
+}
+
+fn symphonia_kind(e: &symphonia::core::errors::Error) -> &'static str {
+    use symphonia::core::errors::Error::*;
+    match e {
+        IoError(_) => "io",
+        DecodeError(_) => "decode",
+        SeekError(_) => "seek",
+        Unsupported(_) => "unsupported",
+        LimitError(_) => "limit",
+        ResetRequired => "reset_required",
     }
 }
 
@@ -76,30 +124,12 @@ impl std::error::Error for MediaError {}
 
 impl From<MediaError> for SparkError {
     fn from(e: MediaError) -> Self {
-        let mut err = SparkError::new(spark_core::ErrorCode::parse(e.code()));
-        match &e {
-            MediaError::Open { path, detail } => {
-                if let Some(p) = path {
-                    err = err.arg(
-                        "path",
-                        ErrorArg::String(Arc::from(p.to_string_lossy().as_ref())),
-                    );
-                }
-                err.arg("detail", ErrorArg::String(Arc::from(detail.as_str())))
-            }
-            MediaError::UnsupportedCodec { codec } => {
-                err.arg("codec", ErrorArg::String(Arc::from(codec.as_str())))
-            }
-            MediaError::Decode { detail } => {
-                err.arg("detail", ErrorArg::String(Arc::from(detail.as_str())))
-            }
-            _ => err,
-        }
+        SparkError::new(spark_core::ErrorCode::parse(e.code())).with_args(e.args())
     }
 }
 
 impl From<symphonia::core::errors::Error> for MediaError {
     fn from(e: symphonia::core::errors::Error) -> Self {
-        MediaError::decode(e.to_string())
+        MediaError::decode_kind(symphonia_kind(&e))
     }
 }
