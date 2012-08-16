1c80c10cde7ce8f21ba238e55c99f1b9b376c644
aster <aster@vers.site>
2012-08-16 05:09:28 +0000

    ✨ Apply script `ScriptMarker` via component descriptor catalog
    Introduce `ComponentDescriptorId` and `ScriptComponentCatalog` so named add/remove can resolve a builtin marker instead of always skipping.

--- projects/gameplay/spark-engine/src/command_apply.rs
+++ projects/gameplay/spark-engine/src/command_apply.rs
@@ -1,13 +1,18 @@
 //! 将脚本命令缓冲提交到 ECS [`World`]。
 //!
-//! 初版：`Spawn` 生成带 [`ScriptArchetypeTag`] 的实体；`Despawn` 按位模式句柄销毁。
-//! 按名增删组件尚需组件描述符槽位，暂记入 [`CommandApplyReport::skipped_named_components`]。
+//! `Spawn` 生成带 [`ScriptArchetypeTag`] 的实体；`Despawn` 按位模式句柄销毁。
+//! 按名增删组件经 [`ScriptComponentCatalog`] 解析为稳定 [`ComponentDescriptorId`]。
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use spark_ecs::{Entity, World};
 
 use crate::command_buffer::ScriptCommand;
+
+/// 脚本侧组件描述符槽位（与宿主登记表顺序一致）。
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ComponentDescriptorId(pub u32);
 
 /// 脚本原型标记组件：提交期占位，游戏层可展开为真实原型。
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -15,11 +20,67 @@ pub struct ScriptArchetypeTag {
     pub name: Arc<str>,
 }
 
+/// 脚本可挂载的空标记组件（验证按名增删路径）。
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+pub struct ScriptMarker;
+
+/// 内建可应用组件名。
+pub const SCRIPT_MARKER_NAME: &str = "ScriptMarker";
+
+/// 脚本组件名 → 描述符槽。
+#[derive(Debug, Clone, Default)]
+pub struct ScriptComponentCatalog {
+    by_name: HashMap<Arc<str>, ComponentDescriptorId>,
+    names: Vec<Arc<str>>,
+}
+
+impl ScriptComponentCatalog {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// 引擎默认：登记 [`SCRIPT_MARKER_NAME`]。
+    pub fn with_builtins() -> Self {
+        let mut catalog = Self::new();
+        catalog.register(SCRIPT_MARKER_NAME);
+        catalog
+    }
+
+    pub fn register(&mut self, name: impl Into<Arc<str>>) -> ComponentDescriptorId {
+        let name = name.into();
+        if let Some(&id) = self.by_name.get(&name) {
+            return id;
+        }
+        let id = ComponentDescriptorId(self.names.len() as u32);
+        self.by_name.insert(Arc::clone(&name), id);
+        self.names.push(name);
+        id
+    }
+
+    pub fn id_of(&self, name: &str) -> Option<ComponentDescriptorId> {
+        self.by_name.get(name).copied()
+    }
+
+    pub fn name_of(&self, id: ComponentDescriptorId) -> Option<&str> {
+        self.names.get(id.0 as usize).map(|s| s.as_ref())
+    }
+
+    pub fn len(&self) -> usize {
+        self.names.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.names.is_empty()
+    }
+}
+
 /// 单次提交报告。
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct CommandApplyReport {
     pub spawned: Vec<Entity>,
     pub despawned: Vec<Entity>,
+    pub added_components: usize,
+    pub removed_components: usize,
     pub skipped_named_components: usize,
     pub failed_despawns: usize,
 }
@@ -28,15 +89,26 @@ impl CommandApplyReport {
     pub fn merge(&mut self, other: CommandApplyReport) {
         self.spawned.extend(other.spawned);
         self.despawned.extend(other.despawned);
+        self.added_components += other.added_components;
+        self.removed_components += other.removed_components;
         self.skipped_named_components += other.skipped_named_components;
         self.failed_despawns += other.failed_despawns;
     }
 }
 
-/// 将一批脚本命令应用到世界。
+/// 将一批脚本命令应用到世界（使用内建组件目录）。
 pub fn apply_script_commands(
     world: &mut World,
     commands: &[ScriptCommand],
+) -> CommandApplyReport {
+    apply_script_commands_with(world, commands, &ScriptComponentCatalog::with_builtins())
+}
+
+/// 将一批脚本命令应用到世界（显式组件目录）。
+pub fn apply_script_commands_with(
+    world: &mut World,
+    commands: &[ScriptCommand],
+    catalog: &ScriptComponentCatalog,
 ) -> CommandApplyReport {
     let mut report = CommandApplyReport::default();
     for cmd in commands {
@@ -55,8 +127,39 @@ pub fn apply_script_commands(
                     report.failed_despawns += 1;
                 }
             }
-            ScriptCommand::AddComponent { .. } | ScriptCommand::RemoveComponent { .. } => {
-                report.skipped_named_components += 1;
+            ScriptCommand::AddComponent { entity, component } => {
+                let e = Entity::from_bits(*entity);
+                match catalog.id_of(component) {
+                    Some(id) if catalog.name_of(id) == Some(SCRIPT_MARKER_NAME) => {
+                        if world.insert(e, ScriptMarker) {
+                            report.added_components += 1;
+                        } else {
+                            report.skipped_named_components += 1;
+                        }
+                    }
+                    Some(_) => {
+                        // 已登记但本 crate 尚无类型体：计入跳过，避免静默当成功。
+                        report.skipped_named_components += 1;
+                    }
+                    None => {
+                        report.skipped_named_components += 1;
+                    }
+                }
+            }
+            ScriptCommand::RemoveComponent { entity, component } => {
+                let e = Entity::from_bits(*entity);
+                match catalog.id_of(component) {
+                    Some(id) if catalog.name_of(id) == Some(SCRIPT_MARKER_NAME) => {
+                        if world.remove::<ScriptMarker>(e).is_some() {
+                            report.removed_components += 1;
+                        } else {
+                            report.skipped_named_components += 1;
+                        }
+                    }
+                    Some(_) | None => {
+                        report.skipped_named_components += 1;
+                    }
+                }
             }
         }
     }
@@ -94,7 +197,7 @@ mod tests {
     }
 
     #[test]
-    fn named_component_edits_are_skipped() {
+    fn unknown_component_edits_are_skipped() {
         let mut world = World::new();
         let report = apply_script_commands(
             &mut world,
@@ -104,5 +207,43 @@ mod tests {
             }],
         );
         assert_eq!(report.skipped_named_components, 1);
+        assert_eq!(report.added_components, 0);
+    }
+
+    #[test]
+    fn script_marker_add_and_remove() {
+        let mut world = World::new();
+        let spawn = apply_script_commands(
+            &mut world,
+            &[ScriptCommand::Spawn {
+                archetype: Arc::from("unit"),
+            }],
+        );
+        let e = spawn.spawned[0];
+        let catalog = ScriptComponentCatalog::with_builtins();
+        assert_eq!(
+            catalog.id_of(SCRIPT_MARKER_NAME),
+            Some(ComponentDescriptorId(0))
+        );
+        let added = apply_script_commands_with(
+            &mut world,
+            &[ScriptCommand::AddComponent {
+                entity: e.to_bits(),
+                component: Arc::from(SCRIPT_MARKER_NAME),
+            }],
+            &catalog,
+        );
+        assert_eq!(added.added_components, 1);
+        assert!(world.get::<ScriptMarker>(e).is_some());
+        let removed = apply_script_commands_with(
+            &mut world,
+            &[ScriptCommand::RemoveComponent {
+                entity: e.to_bits(),
+                component: Arc::from(SCRIPT_MARKER_NAME),
+            }],
+            &catalog,
+        );
+        assert_eq!(removed.removed_components, 1);
+        assert!(world.get::<ScriptMarker>(e).is_none());
     }
 }
